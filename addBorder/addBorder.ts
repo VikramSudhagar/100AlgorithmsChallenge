3f214b940864b133b35eaf5fedf713b5cb296160
VikramSudhagar <69441823+VikramSudhagar@users.noreply.github.com>
2021-08-30 11:31:55 -0700

    init

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,20 @@
 export function addBorder(picture: string[]): string[] {
 
+    const border = picture[0].length + 2;
+
+    var wall: string = "";
+
+    wall = '*'.repeat(border);
+
+    for(let j = 0; j < picture.length; j++) {
+        picture[j] = '*'.concat(picture[j], "*"); 
+    }
+
+    picture.push(wall)
+    picture.unshift(wall);
+
+    return picture;
+
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["abc", "ded"]));
