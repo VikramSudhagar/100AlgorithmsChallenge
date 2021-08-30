3e1935548de7d3c1b9f4d3a9473a230f9429642a
VikramSudhagar <69441823+VikramSudhagar@users.noreply.github.com>
2021-08-29 18:35:36 -0700

    init

--- add/add.ts
+++ add/add.ts
@@ -1,12 +1,22 @@
 export function add(param1: number, param2: number): number {
 
+    return param1 + param2; 
+
 }
 
 export function add2(...param1: number[]): number {
 
-}
-// console.log(add(1, 2));
-// console.log(add(3, 2));
+    let total = 0;
 
-// console.log(add2(1,2,3,4,5));
-// console.log(add2(2,3));
+    param1.forEach(element => {
+        total = total + element;
+    })
+
+    return total;
+
+}
+console.log(add(1, 2));
+console.log(add(3, 2));
+
+console.log(add2(1,2,3,4,5));
+console.log(add2(2,3));
