d95c125487f64b8de42f54f7957d8e5b320da4af
VikramSudhagar <69441823+VikramSudhagar@users.noreply.github.com>
2021-08-29 17:59:43 -0700

    init

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -1,8 +1,16 @@
 export function absoluteValuesSumMinimization(a: number[]): number {
-    return 5;
+
+    let answer = 0;
+
+    if(a.length % 2 !== 0){
+        //odd
+        return answer = a[Math.floor((a.length)/2)];
+    } else {
+        return a[(a.length)/2 - 1];
+    }
 }
 
-// console.log(absoluteValuesSumMinimization([2, 4, 7]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
+console.log(absoluteValuesSumMinimization([2, 4, 7]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
