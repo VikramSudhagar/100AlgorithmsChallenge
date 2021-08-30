aa556afbc36e6007322e378a0543958183158a4d
VikramSudhagar <69441823+VikramSudhagar@users.noreply.github.com>
2021-08-30 12:41:39 -0700

    init

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,23 @@
 export function addTwoDigits(n: any): number {
 
+    const secondDigit = n%10;
+
+    const firstDigit = (n - secondDigit)/10;
+
+    return firstDigit + secondDigit;
 }
 
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(29));
+
+
+//Instructors Solution 
+
+// export function addTwoDigits(n: any): number {
+
+//     const nums = n.toString().split('');
+
+//     return nums.reduce((a: string, b: string) => {
+//         return parseInt(a + b);
+//     })
+    
+// }
