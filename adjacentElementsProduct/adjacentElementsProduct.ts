5f3b894190e906b12add7097ee70a9019caf4db3
VikramSudhagar <69441823+VikramSudhagar@users.noreply.github.com>
2021-08-31 10:43:04 -0700

    init

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,19 @@
 export function adjacentElementsProduct(inputArray: number[]): number {
 
+    var productValue : number = Number.MIN_VALUE;
+
+    let j = 0;
+
+    for(let i = 1; i < inputArray.length; i++) {
+        
+        if(inputArray[i] * inputArray[j] > productValue) {
+            productValue = inputArray[i] * inputArray[j];
+        }
+
+    }
+
+    return productValue;
+
 }
 
-//console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
