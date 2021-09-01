0de39afc597c8d56789b1de3f3f35972a4ba487b
VikramSudhagar <69441823+VikramSudhagar@users.noreply.github.com>
2021-08-31 19:46:33 -0700

    init

--- almostIncreasingSequence/almostIncreasingSequence.ts
+++ almostIncreasingSequence/almostIncreasingSequence.ts
@@ -1,6 +1,22 @@
 export function almostIncreasingSequence(sequence: number[]): boolean {
 
+    //counts everytime there is an element which breaks the increasing sequence
+    let counter = 0;
+    let j = 0;
+
+    for(let i = 1; i < sequence.length; i++){
+        if((sequence[i] - 1) != sequence[j]) {
+            counter++;
+        }
+    }
+
+    if(counter > 1){
+        return false;
+    }
+
+    return true;
+
 }
 
-// console.log(almostIncreasingSequence([1, 3, 2, 1])) 
-// console.log(almostIncreasingSequence([1, 3, 2])) 
+console.log(almostIncreasingSequence([1, 3, 1, 2])) 
+console.log(almostIncreasingSequence([1, 3, 2])) 
