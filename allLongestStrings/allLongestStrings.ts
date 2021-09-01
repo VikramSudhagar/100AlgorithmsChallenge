68f5de2bf4b831531c9deded20370deb4f2a17de
VikramSudhagar <69441823+VikramSudhagar@users.noreply.github.com>
2021-08-31 18:57:26 -0700

    init

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,4 +1,41 @@
 export function allLongestStrings(inputArray: string[]): string[] {
+    //two pointer method
+
+    var biggestLength: number = 0;
+
+    let i = 0; 
+    let j = inputArray.length - 1;
+    console.log(j)
+    console.log(i)
+    var answer: string[] = [];
+
+    while (i < j) {
+
+        if(inputArray[i].length > biggestLength) {
+            biggestLength = inputArray[i].length;
+        }
+
+        if(inputArray[j].length > biggestLength) {
+            biggestLength = inputArray[j].length;
+        }
+        j--;
+        i++;
+    }
+
+    console.log(biggestLength)
+    console.log(inputArray[0])
+
+    let m = 0;
+    let k = 0;
+    for(k ; k < inputArray.length; k++) {
+        if(inputArray[k].length === biggestLength) {
+            answer[m] = inputArray[k];
+            m++;
+        }
+    }
+
+    return answer;
+    
 
 }
 
