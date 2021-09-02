eff698bba4cff042cf510f9f2070a7b1de0f51a7
VikramSudhagar <69441823+VikramSudhagar@users.noreply.github.com>
2021-09-02 12:20:49 -0700

    init

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,22 @@
 export function alternatingSums(a: number[]): number[] {
 
+    var teamOne: number = 0;
+    var teamTwo: number = 0;
+
+    for(let i = 0; i < a.length; i++) {
+
+        if(i % 2 == 0) {
+            teamOne = a[i] + teamOne;
+        } else {
+            teamTwo = a[i] + teamTwo;
+        }
+
+    }
+
+    var answer: number[] = [teamOne, teamTwo];
+
+    return answer;
+
 }
 
 console.log(alternatingSums([50, 60, 60, 45, 70]))
