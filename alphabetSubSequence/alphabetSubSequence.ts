e25a3ce042c8a3f4001c0db77b25fe498fd51baa
VikramSudhagar <69441823+VikramSudhagar@users.noreply.github.com>
2021-09-02 11:18:05 -0700

    init

--- alphabetSubSequence/alphabetSubSequence.ts
+++ alphabetSubSequence/alphabetSubSequence.ts
@@ -1,5 +1,16 @@
 export function alphabetSubsequence(s: string): boolean {
 
+    const alphabet: string[] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+    for(let i = 1; i < s.length; i++) {
+        if((alphabet.indexOf(s[i]) < alphabet.indexOf(s[i-1])) || (alphabet.indexOf(s[i]) == alphabet.indexOf(s[i-1]))) {
+            return false;
+        }
+
+    }
+
+    return true;
+
 }
 
 console.log(alphabetSubsequence('zab'))
